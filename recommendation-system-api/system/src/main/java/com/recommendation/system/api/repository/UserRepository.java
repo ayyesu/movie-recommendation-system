974cdfa35843d9aec345a1eec8e137ee69d6575e
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-23 09:58:22 +0000

    added UserRepository

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/repository/UserRepository.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package com.recommendation.system.api.repository;public interface UserRepository {
+package com.recommendation.system.api.repository;
+
+import com.recommendation.system.api.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
 }
