6b75f0692b8f3d65fdd90cb4a63989740531a9d7
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-23 10:41:24 +0000

    built the CustomUserDetailsService

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/CustomUserDetailsService.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/CustomUserDetailsService.java
@@ -1,2 +1,36 @@
-package com.recommendation.system.api.security;public class CustomUserDetailsService {
+package com.recommendation.system.api.security;
+
+import com.recommendation.system.api.models.Role;
+import com.recommendation.system.api.models.UserEntity;
+import com.recommendation.system.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new User(user.getUsername(), user.getPassword(), mapRolestoAuthorities(user.getRoles()));
+    }
+
+    private Collection<GrantedAuthority> mapRolestoAuthorities(List<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
 }
