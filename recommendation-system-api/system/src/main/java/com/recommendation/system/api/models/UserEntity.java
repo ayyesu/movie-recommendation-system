5b183aa2b453084b7543cf8793411c21e365ec79
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-23 01:42:32 +0000

    added: UserEntity

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/models/UserEntity.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/models/UserEntity.java
@@ -1,2 +1,24 @@
-package com.recommendation.system.api.models;public class UserEntity {
+package com.recommendation.system.api.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
+
 }
