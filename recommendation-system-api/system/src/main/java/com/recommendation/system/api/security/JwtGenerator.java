aeaa4a73e0273d76e40f257cd012c509d861dba6
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-24 22:06:31 +0000

    implemented JwtGenerator

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/JwtGenerator.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/JwtGenerator.java
@@ -1,2 +1,45 @@
-package com.recommendation.system.api.security;public class JwtGenerator {
+package com.recommendation.system.api.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtGenerator {
+
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expiryDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.ES256, SecurityConstants.JWT_SECRET)
+                .compact();
+        return token;
+    }
+
+    public String getUsernameFromToken(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SecurityConstants.JWT_SECRET).parseClaimsJws(token);
+            return true;
+        } catch (Exception ex){
+            throw new AuthenticationCredentialsNotFoundException("JWT not found or incorrect");
+        }
+    }
 }
