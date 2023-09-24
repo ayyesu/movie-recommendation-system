44385c76ad59a779b6f8ecc755013127c388d771
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-24 13:53:04 +0000

    fixed @Service annotation in CustomerUserDetailService

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/CustomUserDetailsService.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/CustomUserDetailsService.java
@@ -4,17 +4,20 @@ import com.recommendation.system.api.models.Role;
 import com.recommendation.system.api.models.UserEntity;
 import com.recommendation.system.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
