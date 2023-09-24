57d70953add5d54401659a8299f32608b39c6188
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-24 16:51:41 +0000

    Setup loginDto

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/dto/LoginDto.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/dto/LoginDto.java
@@ -1,2 +1,19 @@
-package com.recommendation.system.api.dto;public class LoginDto {
+package com.recommendation.system.api.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
