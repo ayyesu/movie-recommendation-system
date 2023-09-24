9b16842e8cbd3883024bdee099f2868b82f65ba3
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-24 13:54:47 +0000

    Simple RegisterDto setup

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/dto/RegisterDto.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/dto/RegisterDto.java
@@ -1,4 +1,17 @@
 package com.recommendation.system.api.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterDto {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
