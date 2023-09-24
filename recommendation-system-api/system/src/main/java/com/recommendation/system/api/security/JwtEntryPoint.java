8ba82963132d84590e7f099382b5d688a75dc074
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-24 22:03:57 +0000

    updated: Security Constant

---
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/JwtEntryPoint.java
@@ -0,0 +1,2 @@
+package com.recommendation.system.api.security;public class JwtEntryPoint {
+}
---
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/SecurityConstants.java
@@ -0,0 +1,7 @@
+package com.recommendation.system.api.security;
+
+public class SecurityConstants {
+
+    public static final Long JWT_EXPIRATION = 70000L;
+    public static final String JWT_SECRET = "ajkjaenuoin";
+}
