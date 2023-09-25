ae7dc4158329a90c3ca948338e6b1807325fa6b8
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-25 10:52:17 +0000

    Setup jwt in AuthController

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/controller/AuthController.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.recommendation.system.api.controller;
 
+import com.recommendation.system.api.dto.AuthResponseDto;
 import com.recommendation.system.api.dto.LoginDto;
 import com.recommendation.system.api.dto.RegisterDto;
 import com.recommendation.system.api.models.Role;
 import com.recommendation.system.api.models.UserEntity;
 import com.recommendation.system.api.repository.RoleRepository;
 import com.recommendation.system.api.repository.UserRepository;
+import com.recommendation.system.api.security.JwtGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,23 +28,26 @@ public class AuthController {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
+    private JwtGenerator jwtGenerator;
 
     @Autowired
     public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository,
-                          RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+                          RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtGenerator jwtGenerator) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtGenerator = jwtGenerator;
     }
 
     @PostMapping("login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        return new ResponseEntity<>("User signin success", HttpStatus.OK);
+        String token = jwtGenerator.generateToken(authentication);
+        return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
     }
 
     @PostMapping("register")
