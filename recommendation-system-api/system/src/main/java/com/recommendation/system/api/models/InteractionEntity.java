87abd920d43b6b932cd01f6c564c1af06b2f653b
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-25 14:59:31 +0000

    added interaction entity

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/models/InteractionEntity.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/models/InteractionEntity.java
@@ -1,2 +1,58 @@
-package com.recommendation.system.api.models;public class InteractionEntity {
+package com.recommendation.system.api.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "interactions", schema = "recommendation_sys")
+public class InteractionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private MovieEntity movie;
+
+    private Integer rating;
+
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public MovieEntity getMovie() {
+        return movie;
+    }
+
+    public void setMovie(MovieEntity movie) {
+        this.movie = movie;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
 }
+
