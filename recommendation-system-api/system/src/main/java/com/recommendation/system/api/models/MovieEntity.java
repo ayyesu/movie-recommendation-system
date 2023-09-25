ef49c16ea1d8c94e5885af8e29807525ad1e23f3
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-25 15:00:09 +0000

    added Movie Entity

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/models/MovieEntity.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/models/MovieEntity.java
@@ -1,2 +1,55 @@
-package com.recommendation.system.api.models;public class MovieEntity {
+package com.recommendation.system.api.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    // Add any other movie-related attributes here
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_genres",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private List<GenreEntity> genres;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<GenreEntity> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenreEntity> genres) {
+        this.genres = genres;
+    }
+
+    // Add getters and setters for other attributes
 }
+
