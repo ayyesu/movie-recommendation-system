7759dfafcb567f4c9b1e5c583f38e8474bda8d4b
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-25 10:40:45 +0000

    fixed jwt secret key error

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/JwtGenerator.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/JwtGenerator.java
@@ -3,14 +3,18 @@ package com.recommendation.system.api.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtGenerator {
+
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     public String generateToken(Authentication authentication){
         String username = authentication.getName();
@@ -21,25 +25,29 @@ public class JwtGenerator {
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.ES256, SecurityConstants.JWT_SECRET)
+                .signWith(key,SignatureAlgorithm.HS512)
                 .compact();
         return token;
     }
 
     public String getUsernameFromToken(String token){
-        Claims claims = Jwts.parser()
-                .setSigningKey(SecurityConstants.JWT_SECRET)
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
     }
 
-    public boolean validateToken(String token){
-        try{
-            Jwts.parser().setSigningKey(SecurityConstants.JWT_SECRET).parseClaimsJws(token);
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
             return true;
-        } catch (Exception ex){
-            throw new AuthenticationCredentialsNotFoundException("JWT not found or incorrect");
+        } catch (Exception ex) {
+            throw new AuthenticationCredentialsNotFoundException("JWT was exprired or incorrect",ex.fillInStackTrace());
         }
     }
 }
