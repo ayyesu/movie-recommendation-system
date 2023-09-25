dd537918204ec4f77e8b3297d06e779acc558a36
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-25 15:55:20 +0000

    removed jwt secret from constants

--- recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/SecurityConstants.java
+++ recommendation-system-api/system/src/main/java/com/recommendation/system/api/security/SecurityConstants.java
@@ -3,5 +3,4 @@ package com.recommendation.system.api.security;
 public class SecurityConstants {
 
     public static final Long JWT_EXPIRATION = 70000L;
-    public static final String JWT_SECRET = "secret";
 }
